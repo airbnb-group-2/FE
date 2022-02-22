cbf2cf1a4c66e674b755eeca84cfbd8a38ffa6d5
Rizki Awenk <adiwirizki207@gmail.com>
2022-02-22 11:29:50 +0700

    add redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../store/reducers';
 
+let composeEnhancers = compose
+let store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)))
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider store = {store} >
+      <div>
+
+      <Component {...pageProps} />
+      </div>
+    </Provider>
+  )
+}
+  
 export default MyApp
