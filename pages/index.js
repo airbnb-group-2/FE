08c727292afc04fe631b21d5678ab787c0af0c3a
Muhammad Yusuf Untung Wahyudi <97287020+myusufuntung@users.noreply.github.com>
2022-02-23 13:47:13 +0700

    Revert "Finished footer component"

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Footer from "../components/footer";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1> Semangat Gan !</h1>
-      <h1 className="">Hello world!</h1>
-      <Footer />
-    </div>
-  );
+    <h1> Semangat Gan !</h1>
+  )
 }
