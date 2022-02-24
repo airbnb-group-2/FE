1fccf330ff964abaeb0f275a097e2b2ad54caa57
unknown <myusufuntung@gmail.com>
2022-02-24 11:49:19 +0700

    Finished footer component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <h1 className='text-blue-700'> Semangat Gan !</h1>
-  )
+    <div>
+      <h1 className="text-blue-700"> Semangat Gan !</h1>
+      <Footer />
+    </div>
+  );
 }
