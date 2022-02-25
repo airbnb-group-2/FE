d49d6c61ccc77099ee5c86232c87b32cc09bfb9c
Aditya Fabio <fabioaditya1995@gmail.com>
2022-02-25 09:32:35 +0700

    homepage fix awal

--- components/homecard.jsx
+++ components/homecard.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 <link rel="stylesheet" href="src/style.css"></link>
 import Image from "next/image";
+import mainImg from "../assets/card.png";
+import Star from "../assets/star.png";
 
 function homecard() {
     return (
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-        <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"/>
-        <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-            <p class="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-            </p>
-        </div>
-        <div class="px-6 pt-4 pb-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-        </div>
+        <div className="bg-[#7F6F62] bg-opacity-70 ml-[30px] mr-[30px] mb-[50px] max-w-sm overflow-hidden shadow-black shadow-md hover:shadow-none rounded-2xl ">
+            <Image className="w-full" src={mainImg} alt="kamar"/>
+            <div class="px-[2vh] py-[2vh]">
+                <div class="font-bold text-lg mb-2">Premium Joglo room</div>
+                <p class="text-gray-900 text-sm mb-1">
+                    <p>2 Guest - 1 Bedroom - 1 Bed</p>
+                    <p>1 Bathroom - Wifi - AC - Kitchen</p>
+                    Free Parking
+                </p>
+                <div>
+                    <Image className="w-full" src={Star} alt="rating"/>
+                </div>
+                <div class="font-bold text-lg mt-1 mb-2">Rp.500.000</div>
+            </div>
         </div>
     );
 }
