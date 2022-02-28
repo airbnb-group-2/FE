a6e21184fadd46e4e4f5efd7d98fa6ea689224bb
Rizki Awenk <adiwirizki207@gmail.com>
2022-02-28 21:31:27 +0700

    Login and Register Done

--- store/reducers/index.jsx
+++ store/reducers/index.jsx
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux"
+import userType from "./userType" 
+
 const rootReducers = combineReducers({
+    userType
 })
 
-export default rootReducers
+export default rootReducers;
